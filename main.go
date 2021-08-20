cea5f2817f451187f92a667e4e044bdf23e07aee
LHaosss <1710713887@qq.com>
2021-08-20 15:12:32 +0800

    20210820 second lihao

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for {
+		fmt.Println("this for testing the using of goreleaser!!!")
+		time.Sleep(time.Duration(1) * time.Second)
+	}
+}
